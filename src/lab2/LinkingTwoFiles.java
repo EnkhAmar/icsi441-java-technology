aa2d767085ba72bd9022e84d0977700aa6ba006e
EnkhAmar <enkhamar1220@gmail.com>
2023-02-15 18:10:27 +0800

    LinkingTwoFiles

--- src/lab2/LinkingTwoFiles.java
+++ src/lab2/LinkingTwoFiles.java
@@ -1,5 +1,14 @@
 package lab2;
 
+// @student: G.Enkh-Amar /18b1num0399/
+/* Exercise 12.19. (Linking two files)
+(Linking two files) Write a program that prompts the user to enter the name of
+an author and the title of one their books. It writes the author’s name to a file
+by using the method written in Programming Exercise 12.15. Their book’s title
+is written at the end of a file Books.txt with the line number of the author’s
+name in Authors.txt inserted before the title. Assume that the title is not
+already stored in Books.txt and the author name is already in Authors.txt.
+ */
 public class LinkingTwoFiles {
     public static void main(String[] args) {
 
