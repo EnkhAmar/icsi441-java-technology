01711d8d99fad7637f4a589d26dd32ac83a53c7c
EnkhAmar <enkhamar1220@gmail.com>
2023-04-12 20:49:42 +0800

    lab 10 - Semaphore , Multithread

--- src/lab10/DonationAccount.java
+++ src/lab10/DonationAccount.java
@@ -7,19 +7,25 @@ import java.util.concurrent.Semaphore;
 
 public class DonationAccount {
     double balance = 0;
-    private Semaphore semaphore;
+    private static Semaphore semaphore = new Semaphore(1);
 
-    public DonationAccount() {
-        semaphore = new Semaphore(1);
+    public DonationAccount() throws InterruptedException {
         // Create a fixed thread pool with maximum three threads
-        ExecutorService executor = Executors.newFixedThreadPool(2);
+//        ExecutorService executor = Executors.newFixedThreadPool(2);
 
         // Submit runnable tasks to the executor
-        executor.execute(new MyTask());
-        executor.execute(new MyTask());
+//        executor.execute(new MyTask());
+//        executor.execute(new MyTask());
+
+        Thread[] threads = new Thread[2];
+        for (int i = 0; i < 2; i++) {
+            threads[i] = new Thread(new MyTask());
+            threads[i].start();
+            threads[i].join();
+        }
 
         // Shut down the executor
-        executor.shutdown();
+//        executor.shutdown();
 
         System.out.println("balance=" + balance);
     }
@@ -44,6 +50,10 @@ public class DonationAccount {
     }
 
     public static void main(String[] args) {
-        new DonationAccount();
+        try {
+            new DonationAccount();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
